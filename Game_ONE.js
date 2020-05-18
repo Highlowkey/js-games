bca56592ec49e96b379c7f1292a775a3eaa037e0
Highlowkey <pdmcelroy1001@gmail.com>
2020-05-18 17:00:27 -0400

    random circle spawning

--- Game_ONE.js
+++ Game_ONE.js
@@ -1,7 +1,18 @@
 var sketchProc=function(processingInstance){ with (processingInstance){
-size(400, 400); 
+size(400, 400);
 frameRate(60);
 
-//Program Code Goes Here
+//---DRAW FUNCTION---
+var draw=function() {
+
+  //---ASTEROID SPAWNING---
+  var asteroid_x = random(1,400);
+  var asteroid_y = random(1,400);
+  var asteroid_rad = random(10,20);
+  ellipse(asteroid_x, asteroid_y, asteroid_rad, asteroid_rad);
+  //-----------------------
+
+}
+//-------------------
 
 }};
